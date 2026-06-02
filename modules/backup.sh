856a28b4cd73bcdcf8067507c51e4af848e60423
oli_dev0 <oli123per@gmail.com>
2026-06-02 16:12:44 +0200

    GM-32 major backup script overhaul

--- modules/backup.sh
+++ modules/backup.sh
@@ -5,7 +5,7 @@
 #	BACKUP SCRIPT																	#
 #	-------------																	#
 #	Purpose:																		#
-#	- Creates a compressed .tar.gz backup of the script's own directory			 	#
+#	- Creates a compressed .tar.gz backup of the source directory				 	#
 #																					#
 #	Usage:																			#
 #	 ./backup.sh																	#
@@ -25,9 +25,47 @@
 
 # CONFIG
 	source "$(dirname "${BASH_SOURCE[0]}")/../libs/bootstrap.sh"
-	load_libs styles animations math
+	load_libs styles animations
 	set_title "💾 BACKUP 💾"
 	required_commands tar stat numfmt
+
+# CHECK IF SOURCE FOLDER EXISTS
+	[[ -d "$GM_BACKUP_SOURCE" ]] || { log_error "source directory does not exist: $GM_BACKUP_SOURCE" >&2; exit 1; }
+
+# CREATE BACKUP FOLDER
+	mkdir -p "$GM_BACKUP_DEST" || { log_error "cannot create backup folder: $GM_BACKUP_DEST" >&2; exit 1; }
+
+# CHECK IF BACKUP FOLDER EXISTS
+	[[ -d "$GM_BACKUP_DEST" ]] || { log_error "destination directory does not exist: $GM_BACKUP_DEST" >&2; exit 1; }
+
+# GET TRUE PATH
+	backup_source="$(cd "$GM_BACKUP_SOURCE" && pwd -P)"
+	backup_dest="$(cd "$GM_BACKUP_DEST" && pwd -P)"
+
+# CHECK IF SOURCE AND BACKUP FOLDER ARE THE SAME
+	[[ "$backup_source" == "$backup_dest" ]] && { log_error "backup source and destination cannot be the same directory" >&2; exit 1; }
+	
+# VARIABLES
+	timestamp="$(date '+%Y-%m-%d_%H-%M')"
+	tar_file="$backup_dest/$timestamp.tar.gz"
+
+# EXCLUSIONS
+	# fixed excludes, can be added with +=
+	tar_excludes=(
+		--exclude="./tmp"
+		--exclude="./cache"
+	)
+
+	# exclude backup folder from being backed up
+		if [[ "$backup_dest" == "$backup_source"/* ]]; then				# if backup dest is inside source
+			backup_relative_path="${backup_dest#"$backup_source"/}"		# then get relative path (this is needed because tar -C)
+			tar_excludes+=(--exclude="./$backup_relative_path")			# add exclusion to array
+		fi
+	
+	# exclude current archive if created inside source (edge case)
+		if [[ "$tar_file" == "$backup_source"/* ]]; then				# if created tar sits inside source
+			tar_excludes+=(--exclude="./${tar_file#"$backup_source"/}")	# exclude it
+		fi
 
 # ASK
 	clearscreen
@@ -37,26 +75,20 @@
 		exit 0
 	fi
 
-# VARIABLES
-	timestamp="$(date '+%Y-%m-%d_%H-%M')"
-	tar_file="$BACKUP_DIR/$timestamp.tar.gz"
-
 # PROGRESS ANIMATION
 	clearscreen
-	log_start "Backup started..."
-	sleep 0.3
+	log_start "Backup started..."; sleep 0.3
 	clearscreen
 	anim_status_bar "Backup in progress...  "; sleep 0.5
-	printf " "
+	# printf " "
 
 # MAKE BACKUP
-	mkdir -p "$BACKUP_DIR"
-	tar --exclude="./${BACKUP_DIR##*/}" --transform='s|^\./||' -czf "$tar_file" -C "$DIR_TO_BACKUP" .
+	tar "${tar_excludes[@]}" --transform='s|^\./||' -czf "$tar_file" -C "$backup_source" .
 
 # GET FILE INFO
-	file_count=$(tar -tzf "$tar_file" | wc -l)
-	file_size=$(stat -c "%s" "$tar_file" | numfmt --to=iec)							# get size in bytes, convert to readable format
-	file_hidden=$(tar -tzf "$tar_file" | grep -c '/\.[^/]*$\|^\.[^/]*$' || true)	# need || true, or else grep will exit with 1 and trap ERR will trigger
+	file_count=$(tar -tvzf "$tar_file" | awk '$1 ~ /^-/ { count++ } END { print count+0 }')
+	file_size=$(stat -c "%s" "$tar_file" | numfmt --to=iec)		# get size in bytes, convert to readable format
+	file_hidden=$(tar -tvzf "$tar_file" | awk '$1 ~ /^-/ && $0 ~ /(^|\/)\.[^\/]/ { count++ } END { print count+0 }')
 
 # SUCCESS TEXT
 	clearscreen
@@ -66,7 +98,7 @@
 	printf '%s\n' "${GREEN}${BOLD}   ✅ Backup completed${RESET}"
 	printf '%s\n' "${GREEN}   ━━━━━━━━━━━━━━━━━━━━${RESET}"
 
-	# printf '%s\n' "${BOLD}${BLUE}   📦 Folder:${RESET} $(basename "$BACKUP_DIR")"
+	printf '%s\n' "${BOLD}${BLUE}   📦 Archive:${RESET} $tar_file"
 	printf '%s\n' "${BOLD}${BLUE}   🕒 Time:${RESET}	${timestamp//_/ at } "
 	printf '%s' "${BOLD}${BLUE}   📄 Files:${RESET}	$file_count - ${file_hidden} hidden "
 	printf '\n%s\t%s\n\n' "${BOLD}${BLUE}   💾 Size:${RESET}" "$file_size"
