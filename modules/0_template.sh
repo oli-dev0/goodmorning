ffd7fb593a82343dcfd914aa13533ab97444225a
oli_dev0 <oli123per@gmail.com>
2026-06-13 00:45:43 +0200

    updated module template

--- modules/0_template.sh
+++ modules/0_template.sh
@@ -4,3 +4,10 @@
 	source "$(dirname "${BASH_SOURCE[0]}")/../libs/bootstrap.sh"
 	set_title "🌤️ MODULE TITLE 🌤️"	# Adjust title
 	required_commands jq			# checks for required commands
+
+main()
+{
+    :
+}
+
+main "$@"
